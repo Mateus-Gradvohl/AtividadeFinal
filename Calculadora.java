1643d92b739561d002dbd7a246a954a2f2b7609f
Mateus Gradvohl <mateusgradvohl@hotmail.com>
2022-11-20 15:44:00 -0300

    Calculadora com Metodos nao implementados

---
+++ Calculadora.java
@@ -0,0 +1,27 @@
+public class Calculadora {
+
+    public double somar() {
+        return 0;
+    }
+    public double subtrair() {
+        return 0;
+    }
+    public double multiplicar() {
+        return 0;
+    }
+    public double dividir(){
+        return 0;
+    }
+    public double exponencial(){
+        return 0;
+    }
+    public double raizQuad(){
+        return 0;
+    }
+    public double piso(){
+        return 0;   
+    }
+    public double teto(){
+        return 0;
+    }
+}
