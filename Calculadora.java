81c3d04f8c0f105b30e979fae8bdc630cc0617bf
UniforArthur <arthur.moraes@edu.unifor.br>
2022-11-21 20:16:27 -0300

    implementando os métodos se soma, multiplicacao e exponencial, na classe Calculadora

--- Calculadora.java
+++ Calculadora.java
@@ -3,19 +3,19 @@ public class Calculadora {
     //METODOS A SEREM IMPLEMENTADOS 
 
     public double somar(double VariavelA, double VariavelB) {
-        return 0;
+        return VariavelA + VariavelB;
     }
     public double subtrair() {
         return 0;
     }
     public double multiplicar(double VariavelA, double VariavelB) {
-        return 0;
+        return VariavelA * VariavelB;
     }
     public double dividir(){
         return 0;
     }
-    public double exponencial(){
-        return 0;
+    public double exponencial(double VariavelA, double VariavelB){
+        return Math.pow(VariavelA, VariavelB);
     }
     public double raizQuad(){
         return 0;
